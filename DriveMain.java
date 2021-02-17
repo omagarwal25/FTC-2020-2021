e0c61edd469a9f29c05f0acb37f232f6bb2a91ee
omagarwal25 <lidagarwal@gmail.com>
2021-02-17 17:07:32 +0000

    debug2

--- DriveMain.java
+++ DriveMain.java
@@ -10,21 +10,20 @@ import java.util.concurrent.TimeUnit;
 
 @TeleOp(name = "DriveMain", group = "")
 public class DriveMain extends LinearOpMode {
-
   private DcMotor leftDrive;
   private DcMotor rightDrive;
   private DcMotor strafeDrive;
-  private DistanceSensor frontDistance;
-  private DistanceSensor backDistance;
+  //private DistanceSensor frontDistance;
+  //private DistanceSensor backDistance;
   //private Servo clawLeft;
   //private Servo clawRight;
-  private Double shootingDeg;
-  private Double shooterTurnConstant;
-  private Double distanceConstant;
-  private CRServo shooter;
-  private CRServo conveyor;
-  private Double distance;
-  private CRServo intake;
+  //private Double shootingDeg;
+  //private Double shooterTurnConstant;
+  //private Double distanceConstant;
+  //private CRServo shooter;
+  //private CRServo conveyor;
+  //private Double distance;
+  //private CRServo intake;
 
   /**
    * This function is executed when this Op Mode is selected from the Driver Station.
@@ -32,7 +31,7 @@ public class DriveMain extends LinearOpMode {
   @Override
   public void runOpMode() {
 
-  	//INIT
+    //INIT
     leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
     rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
     strafeDrive = hardwareMap.get(DcMotor.class, "strafeDrive");
@@ -43,18 +42,18 @@ public class DriveMain extends LinearOpMode {
 
 
     //shooter = hardwareMap.get(CRServo.class, "shooter");
-    conveyor = hardwareMap.get(CRServo.class, "conveyor");
+    //conveyor = hardwareMap.get(CRServo.class, "conveyor");
     //intake = hardwareMap.get(CRServo.class, "intake");
 
-    shooterTurnConstant = 0.01;
-    distanceConstant = 0.10;
+    //shooterTurnConstant = 0.01;
+    //distanceConstant = 0.10;
 
 
     waitForStart();
     if (opModeIsActive()) {
-    	rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-    	strafeDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-    	//shooter.setDirection(DcMotorSimple.Direction.REVERSE);
+      rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+      strafeDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+      //shooter.setDirection(DcMotorSimple.Direction.REVERSE);
 
       leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
       rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -79,35 +78,10 @@ public class DriveMain extends LinearOpMode {
             rightDrive.setPower(gamepad1.right_stick_y);
           }
         }
-
-        if (gamepad1.cross == True) {
-        	conveyor.setPower(1);
-        	//intake.setPower(1);
-        }
-        else {
-        	conveyor.setPower(0);
-          //intake.setPower(0);
-          //shooter.setPower(0);
-        }
-        
-        /*if (gamepad1.triangle == True) {
-        	if (clawRight.getPosition() == 0) {
-        		clawRight.setPosition(1);
-        		clasLeft.setPosition(1);
-        	}
-        	else {
-        		clawRight.setPosition(0);
-        		clawLeft.setPosition(0);
-        	}
-        }*/
-      
         telemetry.addData("LEncoder", leftDrive.getCurrentPosition());
         telemetry.addData("REncoder", rightDrive.getCurrentPosition());
         telemetry.addData("Left Pow", leftDrive.getPower());
         telemetry.addData("Right Pow", rightDrive.getPower());
-        
-        
-        
         telemetry.update();
       }
     }
